b8b754a39752435a16cac51e5f08e3cb1e7a26ea
Will Sather <satherw@vmware.com>
2024-06-27 11:33:15 -0500

    chore: add prettier and remove junk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import {ReactNode} from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,11 +10,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
