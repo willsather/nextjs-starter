09218847c1242b4c9b1fb70f076b336b6c7a1632
willsather <will@sather.ws>
2025-01-02 23:18:34 -0600

    chore: update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
         <li>Nextjs App Router</li>
         <li>Typescript</li>
         <li>Tailwind</li>
-        <li>Eslint</li>
-        <li>Prettier</li>
+        <li>Biome</li>
       </ul>
 
       <p className="fixed top-0 left-0 flex w-full justify-center border-gray-300 border-b bg-gradient-to-b from-zinc-200 pt-8 pb-6 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
